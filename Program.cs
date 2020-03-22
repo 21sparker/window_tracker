9cd57449f6e040185199900cba511f1c66e6d127
Sean <seanparker500@gmail.com>
2020-03-22 12:31:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Collections.Generic;
+using System;
+
+namespace WindowTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] allProcesses = Process.GetProcesses();
+
+            List<string> processesToIgnore = new List<string>();
+            processesToIgnore.Add("explorer");
+            processesToIgnore.Add("ApplicationFrameHost");
+
+            foreach (Process proc in allProcesses)
+            {
+                if (proc.MainWindowHandle != IntPtr.Zero &&
+                    !processesToIgnore.Contains(proc.ProcessName) &&
+                    !String.IsNullOrWhiteSpace(proc.MainWindowTitle) &&
+                    proc.Responding &&
+                    WindowHelpers.WindowPlacementIsVisible(proc.MainWindowHandle))
+                {
+
+                    Console.WriteLine(proc.ProcessName + proc.Id + proc.Responding + ": " + proc.MainWindowTitle + " (" + proc.MainWindowHandle.ToString() + ")");
+                    Console.WriteLine(proc.MainModule.FileName);
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WindowHelpers.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WindowTracker
+{
+    class WindowHelpers
+    {
+        #region Window Placement Is Visible
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
+        private struct WINDOWPLACEMENT
+        {
+            public int length;
+            public int flags;
+            public int showCmd;
+            public System.Drawing.Point ptMinPosition;
+            public System.Drawing.Point ptMaxPosition;
+            public System.Drawing.Point rcNormalPosition;
+        }
+
+        public static bool WindowPlacementIsVisible(IntPtr hWnd)
+        {
+            WINDOWPLACEMENT placement = new WINDOWPLACEMENT();
+            placement.length = Marshal.SizeOf(placement);
+            GetWindowPlacement(hWnd, ref placement);
+
+            return !placement.rcNormalPosition.IsEmpty;
+
+        }
+        #endregion
+
+        #region Window is Visible
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        public static bool WindowIsVisible(IntPtr hWnd)
+        {
+            return IsWindowVisible(hWnd);
+        }
+
+        #endregion
+
+        #region Get Foreground Window Handle
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        private int GetForegroundWindowHandle()
+        {
+            IntPtr hndl = GetForegroundWindow();
+            return (Int32)hndl;
+        }
+        #endregion
+
+        #region Get Time Since Last User Input
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LASTINPUTINFO
+        {
+            public static readonly int SizeOf = Marshal.SizeOf(typeof(LASTINPUTINFO));
+
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 cbSize;
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32 dwTime;
+        }
+        [DllImport("user32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        public static int GetLastInputTime()
+        {
+            int idleTime = 0;
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            int envTicks = Environment.TickCount;
+
+            if (GetLastInputInfo(ref lastInputInfo))
+            {
+                int lastInputTick = (int)lastInputInfo.dwTime;
+                idleTime = envTicks - lastInputTick;
+            }
+
+            return ((idleTime > 0) ? (idleTime / 1000) : 0);
+        }
+        #endregion
+
+        #region Get Process Exe File Path
+        public string GetProcessPath(Process proc)
+        {
+            return proc.MainModule.FileName;
+        }
+        #endregion
+    }
+}
